0de5f013be4b10cfe5f527ceebcb5b8bb8be05bd
ManvelAv <125878791+ManvelAv@users.noreply.github.com>
2024-03-01 21:39:37 +0300

    новый проект#

--- Models/Truck.cs
+++ Models/Truck.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } // Уникальный идентификатор грузовика
         public string Model { get; set; } // Модель грузовика
         public string Make { get; set; } // Марка грузовика
-        public int Year { get; set; } // Год выпуска
+        public int Type { get; set; } // Тип ТС
         public string Description { get; set; } // Описание грузовика
         public decimal DailyRentalRate { get; set; } // Дневная ставка аренды
         public bool IsAvailable { get; set; } // Доступен ли грузовик для аренды
